79c1c83e97a8eddf9056c930f58d31473cd82505
Blayze <blayzestone@protonmail.com>
2020-05-05 14:48:40 -0700

    create card component from api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import UserCard from './components/UserCard';
 
 class App extends React.Component {
+
+  state = {
+    userData: {}
+  }
+
+  componentDidMount() {
+    // axios.get("https://api.github.com/users/blayzestone")
+      // .then(res => {
+      //   this.setState({ userData: res.data });
+      // })
+      // .catch(err => console.log(err));
+    this.setState({
+      userData: JSON.parse(localStorage.getItem("userData"))
+    });
+  }
+
   render() {
     return(
     <div className="App">
-
+      <UserCard userData={this.state.userData}/>
     </div>
     );
   }
---
+++ client/src/components/UserCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Card, CardText,
+  CardTitle, CardSubtitle,
+} from 'reactstrap';
+
+function UserCard(props) {
+  const { name, bio, public_repos, followers, } = props.userData;
+
+  return(
+    <div>
+      <Card>
+        <CardTitle>{name}</CardTitle>
+        <CardSubtitle>Repos: {public_repos}</CardSubtitle>
+        <CardSubtitle>Followers: {followers}</CardSubtitle>
+        <CardText>{bio}</CardText>
+      </Card>
+    </div>
+  );
+}
+
+export default UserCard;
